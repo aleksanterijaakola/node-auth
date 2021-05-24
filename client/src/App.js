e45b41f455dbabe365e4dd548e138916c0e45c92
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-05-24 12:56:09 +0300

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,32 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 
 // Screens
-import PrivateScreen from "./components/screens/PrivateScreen";
-import LoginScreen from "./components/screens/LoginScreen";
-import RegisterScreen from "./components/screens/RegisterScreen";
-import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
-import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
+
+import Register from "./components/Register";
 
 const App = () => {
   return (
     <Router>
-      <div className="app">
-        <Switch>
-          <PrivateRoute exact path="/" component={PrivateScreen} />
-          <Route exact path="/login" component={LoginScreen} />
-          <Route exact path="/register" component={RegisterScreen} />
-          <Route
-            exact
-            path="/forgotpassword"
-            component={ForgotPasswordScreen}
-          />
-          <Route
-            exact
-            path="/passwordreset/:resetToken"
-            component={ResetPasswordScreen}
-          />
-        </Switch>
-      </div>
+      <Switch>
+        <PrivateRoute exact path="/" component={PrivateRoute} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
     </Router>
   );
 };
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/private", require("./routes/private"));
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, () =>
   console.log(`Sever running on port ${PORT}`)
